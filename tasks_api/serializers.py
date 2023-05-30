b47e35c8fcf201e2226ca46d8064a4bf4b033cbe
enzonjagi <amosndungo@gmail.com>
2023-05-30 15:27:18 +0300

    Serialized the API data for it to be readable on the API

---
+++ tasks_api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Data
+
+class DataSerializer(serializers.ModelSerializer):
+    """Enables the API to read data easily"""
+
+    class Meta:
+        model = Data
+        fields = ('task_name', 'task_description')
--- tasks_api/views.py
+++ tasks_api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Data
+from .serializers import DataSerializer
 
 @api_view(['GET'])
 def getData(request):
     # getdata from api
 
-    return Response()
+    app = Data.objects.all()
+    serializer = DataSerializer(app, many=True)
+
+    return Response(serializer.data)

c4a89822fcdf4681434c88d5a4ae41f15eed367c
enzonjagi <amosndungo@gmail.com>
2023-05-30 15:14:36 +0300

    Setting up the view and model for the Tasks Data

--- tasks_api/models.py
+++ tasks_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    """Models for the Api data"""
+
+    task_name = models.CharField(max_length=200)
+    task_description = models.CharField(max_length=500)
